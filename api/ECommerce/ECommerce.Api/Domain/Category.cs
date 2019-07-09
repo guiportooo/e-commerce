b79aec733690ed82259d91a1eb32adc3812f0809
Guilherme Porto <guiportooo@gmail.com>
2019-07-09 19:26:56 -0300

    --amend

--- api/ECommerce/ECommerce.Api/Domain/Category.cs
+++ api/ECommerce/ECommerce.Api/Domain/Category.cs
@@ -10,30 +10,6 @@ namespace ECommerce.Api.Domain
 
     public static class CategoryMapper
     {
-        public static Category ToCategory(this string category)
-        {
-            switch (category.ToLower())
-            {
-                case "books":
-                    return Category.Books;
-                case "eletronics":
-                    return Category.Eletronics;
-                default:
-                    throw new ArgumentException("Invalid category", nameof(category));
-            }
-        }
-
-        public static string ToString(this Category category)
-        {
-            switch (category)
-            {
-                case Category.Books:
-                    return "books";
-                case Category.Eletronics:
-                    return "eletronics";
-                default:
-                    throw new ArgumentException("Invalid category", nameof(category));
-            }
-        }
+        public static Category ToCategory(this string category) => (Category)Enum.Parse(typeof(Category), category);
     }
 }
